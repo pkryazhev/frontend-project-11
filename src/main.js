7dca1d67c22d49ec0b148df64d5ed774920b4b7a
pkryazhev <kryazhevpavel94@gmail.com>
2026-07-16 22:59:16 +0300

    lint-fix

--- src/main.js
+++ src/main.js
@@ -1,44 +1,42 @@
 import './styles.css'
-import { string, setLocale  } from 'yup'
+import { string, setLocale } from 'yup'
 import { proxy } from 'valtio/vanilla'
 import { initView } from './view'
 
-
 const run = () => {
-    const form = document.querySelector('form')
-    const input = document.querySelector('input')
+  const form = document.querySelector('form')
+  const input = document.querySelector('input')
   const state = proxy({
     isValid: true,
-    errorMessage: "OK"
+    errorMessage: 'OK',
   })
 
   setLocale({
     mixed: {
-        default: "Неизвестная ошибка",
-        required: "Не должно быть пустым",
+      default: 'Неизвестная ошибка',
+      required: 'Не должно быть пустым',
     },
     string: {
-        url: "Ссылка должна быть валидным URL",
-    }
+      url: 'Ссылка должна быть валидным URL',
+    },
   })
   const schema = string().required().url()
 
   initView(state)
 
-
   form.addEventListener('submit', (e) => {
     e.preventDefault()
-    const value = input.value;
+    const value = input.value
     schema.validate(value.trim())
-        .then(() => {
-            state.isValid = true
-            state.errorMessage = "OK"
-        }
-        )
-        .catch((err) => {
-            state.isValid = false;
-            state.errorMessage = err.errors[0]
-        })
+      .then(() => {
+        state.isValid = true
+        state.errorMessage = 'OK'
+      },
+      )
+      .catch((err) => {
+        state.isValid = false
+        state.errorMessage = err.errors[0]
+      })
   })
 }
 
--- src/view.js
+++ src/view.js
@@ -3,16 +3,16 @@ import { snapshot, subscribe } from 'valtio/vanilla'
 const input = document.querySelector('input')
 
 const render = (state) => {
-    const snapshotState = snapshot(state)
-    console.log(snapshotState.errorMessage)
-    if (snapshotState.isValid) {
-            input.value = ""
-            input.focus()
-    }
+  const snapshotState = snapshot(state)
+  console.log(snapshotState.errorMessage)
+  if (snapshotState.isValid) {
+    input.value = ''
+    input.focus()
+  }
 }
 
 export const initView = (state) => {
-    subscribe(state, () => {
-        render(state)
-    })
+  subscribe(state, () => {
+    render(state)
+  })
 }
